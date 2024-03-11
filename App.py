4ca54fa18561adfab70f131f11016b24cb672147
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-12 10:21:28 +1300

    added show_all_styles()

--- App.py
+++ App.py
@@ -1,9 +1,23 @@
 #   main application for database
+import sqlite3
 
+#   functions
+def show_all_styles():
+    db = sqlite3.connect("Jade1.db")
+    cursor = db.cursor()
+    sql = "SELECT * FROM Furniture;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    db.close()
+    return results
 
 #   menu
 while True:
-    print("Welcome")
-    choice = input("Press 1 to break")
+    choice = input("Press 1 to show all styles, press 2 to break: ")
+
     if choice == "1":
+        results = show_all_styles()
+        print(results)
+
+    elif choice == "2":
         break
