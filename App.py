d2bbfb3c1ce6aa64196d0609c5e48a96a9f53baf
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-12 10:24:03 +1300

    added show_all_options()

--- App.py
+++ App.py
@@ -11,13 +11,26 @@ def show_all_styles():
     db.close()
     return results
 
+def show_all_options():
+    db = sqlite3.connect("Jade1.db")
+    cursor = db.cursor()
+    sql = "SELECT Furniture.Name, Options.Seats, Options.Width, Options.Depth, Options.Height, Options.Price FROM Options LEFT JOIN Furniture ON Options.Product_ID = Furniture.Product_ID;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    db.close()
+    return results
+
 #   menu
 while True:
-    choice = input("Press 1 to show all styles, press 2 to break: ")
+    choice = input("Press 1 to show all styles, press 2 to show all options, press 3 to break: ")
 
     if choice == "1":
         results = show_all_styles()
         print(results)
 
     elif choice == "2":
+        results = show_all_options()
+        print(results)
+
+    elif choice == "3":
         break
