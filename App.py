8c32cfede1195551dbdf44dd6a4750338eed6395
20042Sarah <162920687+20042Sarah@users.noreply.github.com>
2024-03-12 15:44:44 +1300

    added show_options_for_style function

--- App.py
+++ App.py
@@ -14,17 +14,29 @@ def show_all_styles():
 def show_all_options():
     db = sqlite3.connect("Jade1.db")
     cursor = db.cursor()
-    sql = "SELECT Furniture.Name, Options.Seats, Options.Width, Options.Depth, Options.Height, Options.Price FROM Options LEFT JOIN Furniture ON Options.Product_ID = Furniture.Product_ID;"
+    sql = """SELECT Furniture.Name, Options.Seats, Options.Width, Options.Depth, Options.Height, Options.Price 
+                FROM Options 
+                LEFT JOIN Furniture 
+                ON Options.Product_ID = Furniture.Product_ID;"""
     cursor.execute(sql)
     results = cursor.fetchall()
     db.close()
     return results
 
 def show_options_for_style(Product_ID):
+    db = sqlite3.connect("Jade1.db")
+    cursor = db.cursor()
+    sql = """SELECT Option_ID, Seats, Width, Depth, Height, Price 
+                FROM Options
+                WHERE Product_ID = %s;""" % Product_ID
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    db.close()
+    return results
 
 #   menu
 while True:
-    choice = input("Press 1 to show all styles, press 2 to show all options, press 3 to break: ")
+    choice = input("Press 1 to show all styles, press 2 to show all options, press 3 to show all options for a style, press X to exit: ")
 
     if choice == "1":
         results = show_all_styles()
@@ -35,4 +47,9 @@ while True:
         print(results)
 
     elif choice == "3":
+        product = input("Please type product ID: ")
+        results = show_options_for_style(product)
+        print(results)
+
+    elif choice.lower() == "x":
         break
